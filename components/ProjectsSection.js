d3d48500a815f836bf38adaf423aa66d1387ca59
Felipe Paraizo <fparaizo3@gmail.com>
2026-02-02 17:44:37 -0300

    fix: adjust `project-media-container`

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -375,7 +375,8 @@ const ProjectsSection = () => {
           position: relative;
           overflow: hidden;
           width: 100%;
-          height: 250px;
+          padding-bottom: 56.25%;
+          height: 0;
           display: flex;
           align-items: center;
           justify-content: center;
